ae7f28954af1a2d34b0e6cb32611bc1a973d7277
Amir <amirgolpaa24@gmail.com>
2020-04-25 13:45:32 +0430

    set debug value

--- bookshare/settings.py
+++ bookshare/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SADBOOKSHARE_DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True #os.environ.get('SADBOOKSHARE_DJANGO_DEBUG')
+DEBUG = (os.environ.get('SADBOOKSHARE_DJANGO_DEBUG') == "True")
 
 ALLOWED_HOSTS = [
     'sadbookshare.herokuapp.com',
