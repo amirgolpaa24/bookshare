7f3f9580403c1e8f6df703d7419d2aa281212995
Amir <amirgolpaa24@gmail.com>
2020-04-26 18:09:37 +0430

    persian messages added

--- account/api/views.py
+++ account/api/views.py
@@ -1,5 +1,7 @@
 from datetime import datetime, timedelta
 
+from bookshare.settings import MSG_LANGUAGE
+
 from django.contrib.auth import authenticate
 from django.contrib.sites.shortcuts import get_current_site
 from django.core.mail import EmailMessage, send_mail
@@ -24,6 +26,37 @@ from .serializers import (ChangePasswordSerializer, EditedUserSerializer,
                           UserSerializer)
 
 
+# messages:
+MSG_NO_EMAIL =                  {'Persian': 'ایمیل ارائه نشده است', 'English': 'No email was provided!'}[MSG_LANGUAGE]
+MSG_NO_USERNAME =               {'Persian': 'نام کاربری ارائه نشده است', 'English': 'No username was provided!'}[MSG_LANGUAGE]
+MSG_NO_FIRSTNAME =              {'Persian': 'نام کوچک ارائه نشده است', 'English': 'No first name was provided!'}[MSG_LANGUAGE]
+MSG_NO_LASTNAME =               {'Persian': 'نام خانوادگی ارائه نشده است', 'English': 'No last name was provided!'}[MSG_LANGUAGE]
+MSG_NO_PASSWORD =               {'Persian': 'رمز عبور ارائه نشده است', 'English': 'No password was provided!'}[MSG_LANGUAGE]
+MSG_NO_PASSWORDCONFIRMATION =   {'Persian': 'تکرار رمز عبور ارائه نشده است', 'English': 'No password confirmation was provided!'}[MSG_LANGUAGE]
+MSG_DUPLICATE_EMAIL =           {'Persian': 'متاسفانه کاربری با این ایمیل وجود دارد', 'English': 'Sorry, user with this email already exists!'}[MSG_LANGUAGE]
+MSG_DUPLICATE_USERNAME =        {'Persian': 'متاسفانه کاربری با این نام کاربری وجود دارد', 'English': 'Sorry, user with this username already exists!'}[MSG_LANGUAGE]
+MSG_NONMATH_PASSWORDS =         {'Persian': 'رمز عبور و تکرار آن یکسان نیستند', 'English': 'Passwords must match!'}[MSG_LANGUAGE]
+MSG_INVALID_FIELDS =            {'Persian': 'ورودی (های) نامعتبر', 'English': 'invalid fields'}[MSG_LANGUAGE]
+MSG_INVALID_LINK =              {'Persian': 'لینک فعال سازی نامعتبر است', 'English': 'Activation link is invalid!'}[MSG_LANGUAGE]
+MSG_WRONG_USERNAMEPASSWORD =    {'Persian': 'نام کاربری یا رمز عبور اشتباه است', 'English': 'Wrong username or password!'}[MSG_LANGUAGE]
+MSG_WRONG_OLDPASSWORD =         {'Persian': 'رمز عبور قدیمی اشتباه است', 'English': "The old password is wrong!"}[MSG_LANGUAGE]
+MSG_NONEXISTANT_USERNAME =      {'Persian': 'چنین کاربری وجود ندارد', 'English': 'There is no such username!'}[MSG_LANGUAGE]
+MSG_NONVERIFIED_EMAIL =         {'Persian': 'این ایمیل فاقد اعتبار کاربری است', 'English': 'This email is not verified yet!'}[MSG_LANGUAGE]
+MSG_CANNOT_RETRIEVE =           {'Persian': 'متاسفانه بازیابی رمز عبور فعلا برای شما ممکن نمی باشد', 'English': 'Sorry, you cannot retrieve your account for now.'}[MSG_LANGUAGE]
+MSG_NO_CHANGES =                {'Persian': 'هیچ تغییری اعمال نگردید', 'English': 'No changes have been made.'}[MSG_LANGUAGE]
+
+MSG_REGISTER_SUCCESS =          {'Persian': 'شما با موفقیت ثبت نام کردید', 'English': 'You have successfully registered your account.'}[MSG_LANGUAGE]
+MSG_LOGIN_SUCCESS =             {'Persian': 'شما با موفقیت وارد حساب کاربری خود شدید', 'English': 'You successfully logged in to your account.'}[MSG_LANGUAGE]
+MSG_EDIT_SUCCESS =              {'Persian': 'شما با موفقیت اطلاعات حساب خود را تغییر دادید', 'English': 'You have successfully updated your account.'}[MSG_LANGUAGE]
+MSG_CHANGEPASSWORD_SUCCESS =    {'Persian': 'شما با موفقیت رمز عبور خود را تغییر دادید', 'English': 'You have successfully changed your password.'}[MSG_LANGUAGE]
+MSG_RESETPASSWORD_SUCCESS =     {'Persian': 'رمز عبور شما با موفقیت تغییر کرد.\n رمز عبور جدید به ایمیل شما فرستاده شده است.', 'English': 'Your password has successfully changed;\nWe sent your new password to your email account.'}[MSG_LANGUAGE]
+
+
+
+
+
+
+
 @api_view(['POST', ])
 @permission_classes([])
 @authentication_classes([])
@@ -41,32 +74,32 @@ def api_register_user_view(request):
         image = request.data.get('image', '0_no_image_provided_0')
 
         if email == '0_no_email_provided_0' or email == '':
-            data['message'] = 'No email was provided!'
+            data['message'] = MSG_NO_EMAIL
             return Response(data, status.HTTP_400_BAD_REQUEST)
         if username == '0_no_username_provided_0' or username == '':
-            data['message'] = 'No username was provided!'
+            data['message'] = MSG_NO_USERNAME
             return Response(data, status.HTTP_400_BAD_REQUEST)
         if first_name == '0_no_first_name_provided_0' or first_name == '':
-            data['message'] = 'No first name was provided!'
+            data['message'] = MSG_NO_FIRSTNAME
             return Response(data, status.HTTP_400_BAD_REQUEST)
         if last_name == '0_no_last_name_provided_0' or last_name == '':
-            data['message'] = 'No last name was provided!'
+            data['message'] = MSG_NO_LASTNAME
             return Response(data, status.HTTP_400_BAD_REQUEST)
         if password == '0_no_password_provided_0' or password == '':
-            data['message'] = 'No password was provided!'
+            data['message'] = MSG_NO_PASSWORD
             return Response(data, status.HTTP_400_BAD_REQUEST)
         if password_confirmation == '0_no_password_confirmation_provided_0' or password_confirmation == '':
-            data['message'] = 'No password confirmation was provided!'
+            data['message'] = MSG_NO_PASSWORDCONFIRMATION
             return Response(data, status.HTTP_400_BAD_REQUEST)
         
         if validate_email(email) is not None:
-            data['message'] = 'Sorry, user with this email already exists!'
+            data['message'] = MSG_DUPLICATE_EMAIL
             return Response(data, status.HTTP_400_BAD_REQUEST)
         if validate_username(username) is not None:
-            data['message'] = 'Sorry, user with this username already exists!'
+            data['message'] = MSG_DUPLICATE_USERNAME
             return Response(data, status.HTTP_400_BAD_REQUEST)
         if password != password_confirmation:
-            data['message'] = 'Passwords must match!'
+            data['message'] = MSG_NONMATH_PASSWORDS
             return Response(data, status.HTTP_400_BAD_REQUEST)
 
         # lowercase email:
@@ -99,7 +132,7 @@ def api_register_user_view(request):
         
         else:
             data = serializer.errors
-            data['message'] = 'invalid fields'
+            data['message'] = MSG_INVALID_FIELDS
             return Response(data, status.HTTP_400_BAD_REQUEST)
 
 
@@ -118,7 +151,7 @@ def activate(request, uidb64, token):
         user.save()
 
         data = {}
-        data['message'] = 'You have successfully registered your account.'
+        data['message'] = MSG_REGISTER_SUCCESS
         token = Token.objects.get(user=user).key
         data['token'] = token
         data['username'] = user.username
@@ -131,7 +164,7 @@ def activate(request, uidb64, token):
 
     else:
         user.is_active = False
-        return Response({'message': 'Activation link is invalid!'}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({'message': MSG_INVALID_LINK}, status=status.HTTP_400_BAD_REQUEST)
 
 
 def get_user_profile_image(user):
@@ -183,42 +216,6 @@ def is_user_expired(user):
     return True
 
 
-# @api_view(['POST', ])
-# @permission_classes([])
-# @authentication_classes([])
-# def login_api(request):
-#     username = request.data.get('username')
-#     password = request.data.get('password')
-#     data = {}
-
-#     data['username'] = str(username)
-#     data['password'] = str(password)
-#     return Response(data, status=status.HTTP_200_OK)
-
-    # user = authenticate(username=username, password=password)
-    
-    # if user:
-    #     try:
-    #         token = Token.objects.get(user=user)
-    #     except Token.DoesNotExist:
-    #         token = Token.objects.create(user=user)
-    #     data['message'] = 'You successfully logged in to your account.'
-    #     data['token'] = token.key
-    #     data['username'] = user.username
-    #     data['email'] = user.email
-    #     data['first_name'] = user.first_name
-    #     data['last_name'] = user.last_name
-    #     data['image'] = get_user_profile_image(user)
-    #     return Response(data, status=status.HTTP_200_OK)
-    # else:
-    #     data['message'] = 'Wrong username or password'
-    #     data['sent_un'] = str(username)
-    #     data['sent_pw'] = str(password)
-    #     data['user'] = str(user)
-    #     return Response(data, status.HTTP_400_BAD_REQUEST)
-
-
-
 class ObtainAuthTokenView(APIView):
     authentication_classes = []
     permission_classes = []
@@ -229,10 +226,10 @@ class ObtainAuthTokenView(APIView):
 
         data = {}
         if username == '0_no_username_provided_0' or username == '':
-            data['message'] = 'No username was provided!'
+            data['message'] = MSG_NO_USERNAME
             return Response(data, status.HTTP_400_BAD_REQUEST)
         if password == '0_no_password_provided_0' or password == '':
-            data['message'] = 'No password was provided!'
+            data['message'] = MSG_NO_PASSWORD
             return Response(data, status.HTTP_400_BAD_REQUEST)
 
         user = authenticate(username=username, password=password)
@@ -242,7 +239,7 @@ class ObtainAuthTokenView(APIView):
                 token = Token.objects.get(user=user)
             except Token.DoesNotExist:
                 token = Token.objects.create(user=user)
-            data['message'] = 'You successfully logged in to your account.'
+            data['message'] = MSG_LOGIN_SUCCESS
             data['token'] = token.key
             data['username'] = user.username
             data['email'] = user.email
@@ -251,10 +248,7 @@ class ObtainAuthTokenView(APIView):
             data['image'] = get_user_profile_image(user)
             return Response(data, status=status.HTTP_200_OK)
         else:
-            data['message'] = 'Wrong username or password'
-            data['sent_un'] = str(username)
-            data['sent_pw'] = str(password)
-            data['user'] = str(user)
+            data['message'] = MSG_WRONG_USERNAMEPASSWORD
             return Response(data, status.HTTP_400_BAD_REQUEST)
 
 
@@ -266,10 +260,10 @@ def api_account_properties_view(request, username):
         try:
             user = User.objects.get(username=username)
         except User.DoesNotExist:
-            return Response({'message': 'There is no such username!'}, status=status.HTTP_404_NOT_FOUND)
+            return Response({'message': MSG_NONEXISTANT_USERNAME}, status=status.HTTP_404_NOT_FOUND)
 
         if not user.is_active:
-            return Response({'message': 'There is no such username!'}, status=status.HTTP_404_NOT_FOUND)
+            return Response({'message': MSG_NONEXISTANT_USERNAME}, status=status.HTTP_404_NOT_FOUND)
 
         requester = request.user
         is_outsider = (user != requester)
@@ -285,12 +279,11 @@ def api_account_properties_view(request, username):
 def api_edit_account_view(request):
 
     if request.method == 'PUT':
-        print('\n-------------------\n')
         user = request.user
 
         editable_fields = ['first_name', 'last_name', 'username']
         if not any([field in request.data for field in editable_fields]):
-            return Response(data={'message': 'No changes have been made.'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(data={'message': MSG_NO_CHANGES}, status=status.HTTP_400_BAD_REQUEST)
         
         request_data = {}
         for field in editable_fields:
@@ -301,10 +294,10 @@ def api_edit_account_view(request):
         
         if serializer.is_valid():
             serializer.save()
-            return Response(data={'message': 'You have successfully updated your account.'}, status=status.HTTP_200_OK)
+            return Response(data={'message': MSG_EDIT_SUCCESS}, status=status.HTTP_200_OK)
         else:
             data = serializer.errors
-            data['message'] = 'invalid fields'
+            data['message'] = MSG_INVALID_FIELDS
             return Response(data, status.HTTP_400_BAD_REQUEST)
 
 
@@ -324,24 +317,25 @@ class ChangePasswordView(UpdateAPIView):
         if serializer.is_valid():
             # Check old password
             if not self.object.check_password(serializer.data.get("old_password")):
-                return Response({"message": "The old password is wrong!"}, status=status.HTTP_400_BAD_REQUEST)
+                return Response({"message": MSG_WRONG_OLDPASSWORD}, status=status.HTTP_400_BAD_REQUEST)
 
             # confirm the new passwords match
             new_password = serializer.data.get("new_password")
             confirm_new_password = serializer.data.get("new_password_confirmation")
             if new_password != confirm_new_password:
-                return Response({"message": "New passwords must match!"}, status=status.HTTP_400_BAD_REQUEST)
+                return Response({"message": MSG_NONMATH_PASSWORDS}, status=status.HTTP_400_BAD_REQUEST)
 
             # set_password also hashes the password that the user will get
             self.object.set_password(serializer.data.get("new_password"))
             self.object.save()
-            return Response({"message": "You have successfully changed your password."}, status=status.HTTP_200_OK)
+            return Response({"message": MSG_CHANGEPASSWORD_SUCCESS}, status=status.HTTP_200_OK)
         else:
             serializer_errors = serializer.errors
             error_message = ''
             for field in ['old_password', 'new_password', 'new_password_confirmation']:
                 if field in serializer_errors:
                     error_message += 'The ' + field.replace('_', ' ')  + ' is required!\n'
+            
             if error_message.endswith('\n'):
                 error_message = error_message[:len(error_message) - 1]
             data = {'message': error_message}
@@ -359,22 +353,22 @@ def api_reset_password_view(request):
         data = {}
         
         if email == '0_no_email_provided_0' or email == '':
-            data['message'] = 'No email was provided!'
+            data['message'] = MSG_NO_EMAIL
             return Response(data, status=status.HTTP_400_BAD_REQUEST)
 
         user = None
         try:
             user = User.objects.get(email=email)
         except User.DoesNotExist:
-            data['message'] = 'This email is not verified yet!'
+            data['message'] = MSG_NONVERIFIED_EMAIL
             return Response(data, status=status.HTTP_400_BAD_REQUEST)
         
         if (not user.is_active):
-            data['message'] = 'This email is not verified yet!'
+            data['message'] = MSG_NONVERIFIED_EMAIL
             return Response(data, status=status.HTTP_400_BAD_REQUEST)
         
         if user.last_retrieval is not None and (datetime.now() - user.last_retrieval) < timedelta(hour=1):
-            data['message'] = 'Sorry, you cannot retrieve your account for now.'
+            data['message'] = MSG_CANNOT_RETRIEVE
             return Response(data, status=status.HTTP_400_BAD_REQUEST)
 
         # if ok:
@@ -396,4 +390,4 @@ def api_reset_password_view(request):
         email_destination = email
         EmailMessage(mail_subject, mail_message, to=[email_destination]).send()
         
-        return Response({'message': 'Your password has successfully changed;\nWe sent your new password to your email account.'}, status=status.HTTP_200_OK)
+        return Response({'message': MSG_RESETPASSWORD_SUCCESS}, status=status.HTTP_200_OK)
--- bookshare/settings.py
+++ bookshare/settings.py
@@ -138,6 +138,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
+MSG_LANGUAGE = 'Persian'
 
 TIME_ZONE = 'UTC'
 
