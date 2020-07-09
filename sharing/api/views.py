eb066b3a6f2981d770129c41a2452ae3cccf8a49
Amir <amirgolpaa24@gmail.com>
2020-07-09 22:05:05 +0430

    sprint 4 debugging

--- sharing/api/views.py
+++ sharing/api/views.py
@@ -258,9 +258,8 @@ def api_deliver_book_to_borrower_view(request, exchange_slug):
             if isinstance(delivery_result, bool):
                 response_data['message'] = MSG_INVALID_STATE
             else:
-
                 mail_subject = 'Delivery Result'
-                mail_message = delivery_result
+                mail_message = str(delivery_result)
                 email_destination = "amirgolpaa24@gmail.com"
                 EmailMessage(mail_subject, mail_message, to=[email_destination]).send()
 
