9b1cfa6d4b0dcae63237e2d192f2fc61c1aa96a1
Amir <amirgolpaa24@gmail.com>
2020-07-10 16:33:48 +0430

    sprint 4 - search book no token

--- search/api/views.py
+++ search/api/views.py
@@ -23,7 +23,7 @@ from .serializers import BookResultSerializer, UserResultSerializer
 
 @api_view(['PUT', ])
 @permission_classes([])
-@authentication_classes([])
+@authentication_classes([TokenAuthentication])
 def api_search_book_view(request):
 
     if request.method == 'PUT':        
